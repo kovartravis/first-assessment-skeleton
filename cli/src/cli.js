44daac1820b426b9449decb68951efc163f1a6ff
unknown <kovartravis@gmail.com>
2017-11-01 11:39:32 -0500

    client can accept host and port args, server can boradcast messages

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -6,17 +6,21 @@ import { Message } from './Message'
 export const cli = vorpal()
 
 let username
+let host = 'localhost'
+let port = 8080
 let server
 
 cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
 
 cli
-  .mode('connect <username>')
-  .delimiter(cli.chalk['green']('connected>'))
+  .mode('connect <username> [host] [port]')
+  .delimiter(cli.chalk['green']('connected>')) 
   .init(function (args, callback) {
     username = args.username
-    server = connect({ host: 'localhost', port: 8080 }, () => {
+    if(args.host) host = args.host
+    if(args.port) port = args.port
+    server = connect({ host, port }, () => {
       server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       callback()
     })
@@ -37,7 +41,10 @@ cli
       server.end(new Message({ username, command }).toJSON() + '\n')
     } else if (command === 'echo') {
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
-    } else {
+    } else if (command === 'broadcast'){
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
+    } 
+    else {
       this.log(`Command <${command}> was not recognized`)
     }
 
